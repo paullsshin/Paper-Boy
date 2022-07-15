3a233ac9a896f990fd0f6679e8e0983a02611e48
jvanosdol <j.vanosdol99@yahoo.com>
2022-07-14 20:55:11 -0700

    added about page

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,20 @@
 // FREE NEWS API - BY COUNTRY
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'e5f47a5545mshf7b0b0480e8a458p1e6668jsn6b3694c80c47',
-// 		'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
-// 	}
-// };
 
-// submitBtn.addEventListener('click', function () {
-//     fetch('https://free-news.p.rapidapi.com/v1/search?q=Canada', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-// });
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e5f47a5545mshf7b0b0480e8a458p1e6668jsn6b3694c80c47',
+		'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
+	}
+};
+
+submitBtn.addEventListener('click', function () {
+    fetch('https://free-news.p.rapidapi.com/v1/search?q=Canada', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+});
 
 
 // BREAKING NEWS API
